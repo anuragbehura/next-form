c68bc6f8d738f2c0257f6134eddbfb0873e32f98
Anurag Behura <behura960@gmail.com>
2025-01-02 13:10:57 +0530

    error solved

--- app/(home)/(pages)/builder/[slug]/page.tsx
+++ app/(home)/(pages)/builder/[slug]/page.tsx
@@ -1,22 +1,14 @@
-
 import { GetFormBySlug } from '@/actions/form';
 import FormBuilder from '@/components/FormBuilder';
 import React from 'react';
 
-async function BuilderPage({params}:{
-  params: {
-    slug: string;
-  }
-}) {
-  const { slug } = params;
-  // console.log("Slug received in BuilderPage:", slug);
+async function BuilderPage({ params }: { params: { slug: string } }) {
+  const { slug } = params; // Destructure the slug from params
   const form = await GetFormBySlug(slug);
   if (!form) {
     throw new Error("form not found");
   }
-  return (
-    <FormBuilder form={form} />
-  )
+  return <FormBuilder form={form} />;
 }
 
-export default BuilderPage
+export default BuilderPage;
